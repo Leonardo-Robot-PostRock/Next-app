700c37c495cb02bb8625d3c8159709b289054abf
Leonardo <leonardo38361@gmail.com>
2023-11-24 17:26:32 -0300

    Componente Navbar

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
+import { Navbar } from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +15,12 @@ export default function About() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+
+			<Navbar />
+
 			<main className={`${styles.main} ${inter.className}`}>
+				<h1>About Page</h1>
+
 				<h1 className={styles.title}>
 					Ir a <Link href='/'>Home</Link>
 				</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
+import { Navbar } from '../components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +18,14 @@ export default function Home() {
 			<Head>
 				<meta name='keywords' content='Leonardo, portfolio, frontend' />
 			</Head>
+
+			<Navbar />
+
 			<main className={`${styles.main} ${inter.className}`}>
+				<h1>Home Page</h1>
+
 				<h1 className={styles.title}>
-					Ir a <Link href="/about">About</Link>
+					Ir a <Link href='/about'>About</Link>
 				</h1>
 
 				<div className={styles.description}>

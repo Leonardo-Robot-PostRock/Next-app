13e040003bb150fb66da411802ed7268ac67fac5
Leonardo <leonardo38361@gmail.com>
2023-11-24 23:49:06 -0300

    eliminacion de importaciones

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,10 +1,8 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
 import styles from '../../components/layouts/MainLayout.module.css';
 import Link from 'next/link';
 import { MainLayout } from '@/components/layouts/MainLayout';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function contactPage() {
 	return (

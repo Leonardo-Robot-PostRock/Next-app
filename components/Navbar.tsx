f83927e8d85f4d30034ad43b03d7c9bb77f451b2
Leonardo <leonardo38361@gmail.com>
2023-11-24 22:40:17 -0300

    Uso de componente personalizado

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
 import React from 'react';
 import styles from './Navbar.module.css';
+import { ActiveLink } from './ActiveLink';
 
 export const Navbar = () => {
 	return (
 		<nav className={styles['menu-container']}>
-			<Link href='/'>Home</Link>
-			<Link href='/about'>About</Link>
-			<Link href='/contact'>Contact</Link>
+			<ActiveLink href='/' text='Home' />
+			<ActiveLink href='/about' text='About' />
+			<ActiveLink href='/contact' text='Contact' />
 		</nav>
 	);
 };

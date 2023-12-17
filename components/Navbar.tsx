646c035ced84cb72d8ff4ecfe55325ecff4e37d0
Leonardo <leonardo38361@gmail.com>
2023-12-17 00:04:00 -0300

    Modificacion de navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,34 @@ import styles from './Navbar.module.css';
 import { ActiveLink } from './ActiveLink';
 
 export const Navbar = () => {
+	interface menuItems {
+		text: string;
+		href: string;
+	}
+
+	const menuItems: menuItems[] = [
+		{
+			text: 'Home',
+			href: '/',
+		},
+		{
+			text: 'About',
+			href: '/about',
+		},
+		{
+			text: 'Contact',
+			href: '/contact',
+		},
+		{
+			text: 'Pricing',
+			href: '/pricing',
+		},
+	];
 	return (
 		<nav className={styles['menu-container']}>
-			<ActiveLink href='/' text='Home' />
-			<ActiveLink href='/about' text='About' />
-			<ActiveLink href='/contact' text='Contact' />
+			{menuItems.map(({ text, href }) => (
+				<ActiveLink key={href} href={href} text={text} />
+			))}
 		</nav>
 	);
 };
